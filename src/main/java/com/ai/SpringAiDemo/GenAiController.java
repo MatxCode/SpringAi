4049491dea10ae0b532e01ca54522b5e130408a0
mateo <letertre@et.esiea.fr>
2025-03-20 23:05:54 +0100

    add gpt-4o

--- src/main/java/com/ai/SpringAiDemo/GenAiController.java
+++ src/main/java/com/ai/SpringAiDemo/GenAiController.java
@@ -16,4 +16,9 @@ public class GenAiController {
     public String getResponse(@RequestParam String prompt) {
         return chatService.getResponse(prompt);
     }
+
+    @GetMapping("ask-ai-options")
+    public String getResponseOptions(@RequestParam String prompt) {
+        return chatService.getResponseOptions(prompt);
+    }
 }
