1f310d796b9042f7d35fd4cb9fc970c3505430e5
mateo <letertre@et.esiea.fr>
2025-03-23 22:45:21 +0100

    finish frontend connected to backend

--- spring-ai-react/src/components/RecipeGenerator.js
+++ spring-ai-react/src/components/RecipeGenerator.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RecipeGenerator() {
+    const [ingredients, setIngredients] = useState("");
+    const [cuisine, setCuisine] = useState("any");
+    const [dietaryRestrictions, setDietaryRestrictions] = useState("");
+    const [recipe, setRecipe] = useState("");
+
+    const createRecipe = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/recipe-creator?ingredients=${ingredients}&dietaryRestrictions=${dietaryRestrictions}&cuisine=${cuisine}`)
+            const data = await response.text();
+            console.log(data);
+            setRecipe(data);
+        } catch (error) {
+            console.error("Error generating recipe : ", error)
+        }
+    }
+
     return (
         <div>
             <h2>Create a Recipe</h2>
+            <input
+                type="text"
+                placeholder="Enter ingredients (comma separated)"
+                value={ingredients}
+                onChange={(e) => setIngredients(e.target.value)}
+            />
+
+            <input
+                type="text"
+                placeholder="Enter cuisine type"
+                value={cuisine}
+                onChange={(e) => setCuisine(e.target.value)}
+            />
+
+            <input
+                type="text"
+                placeholder="Enter dietary restrictions"
+                value={dietaryRestrictions}
+                onChange={(e) => setDietaryRestrictions(e.target.value)}
+            />
+
+            <button onClick={createRecipe}>Create recipe</button>
+
+            <div className="output">
+                <pre className="recipe-text">{recipe}</pre>
+            </div>
         </div>
     );
 }
